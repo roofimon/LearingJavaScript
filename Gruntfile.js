86ccc634c6e5b750eb95a36a5d8e239d66746070
roofimon <roofimon@gmail.com>
2014-07-08 22:35:48 +0700

    Add grunt karma

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,9 +7,17 @@ module.exports = function(grunt) {
         jshint: {
             // You get to make the name
             // The paths tell JSHint which files to validate
-            myFiles: ['src/**/*.js',]
+            myFiles: ['src/**/*.js', 'test/**/*.js']
+        },
+
+        karma: {
+            unit: {
+                configFile: 'karma.conf.js',
+                autoWatch: true
+            }
         }
     });
     // Each plugin must be loaded following this pattern
     grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-karma');
 };
